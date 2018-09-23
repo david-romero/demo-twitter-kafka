4f29c77528da1eb887fc94f8f86172dc0b08a0de
David Romero <davromalc@gmail.com>
2018-09-23 11:07:15 +0200

    Divide step 4 into four substeps

--- producer/src/main/java/com/davromalc/demotwitterkafka/DemoTwitterKafkaProducerApplication.java
+++ producer/src/main/java/com/davromalc/demotwitterkafka/DemoTwitterKafkaProducerApplication.java
@@ -4,12 +4,11 @@ import java.util.Properties;
 
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.clients.producer.ProducerRecord;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import com.google.common.base.Charsets;
 
 import lombok.extern.slf4j.Slf4j;
 import twitter4j.FilterQuery;
@@ -27,33 +26,27 @@ public class DemoTwitterKafkaProducerApplication {
 	public static void main(String[] args) {
 		// Kafka config
 		Properties properties = new Properties();
-		properties.put("bootstrap.servers", "localhost:9092");
-		properties.put("serializer.class", "kafka.serializer.StringEncoder");
-		properties.put("client.id", "demo-twitter-kafka-application-producer");
-		properties.put("key.serializer", StringSerializer.class.getName());
-		properties.put("value.serializer", StringSerializer.class.getName());
+		properties.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092"); //Kafka cluster hosts.
+		properties.put(ProducerConfig.CLIENT_ID_CONFIG, "demo-twitter-kafka-application-producer"); // Group id
+		properties.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName()); 
+		properties.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
 		Producer<String, String> producer = new KafkaProducer<>(properties);
 
 		// Twitter Stream
 		final TwitterStream twitterStream = new TwitterStreamFactory().getInstance();
-		final FilterQuery tweetFilterQuery = new FilterQuery();
-		tweetFilterQuery.track(new String[] { "Java" });
-
 		final StatusListener listener = new StatusListener() {
 			public void onStatus(Status status) {
 				final long likes = getLikes(status);
-				final String tweet = getTweet(status);
+				final String tweet = status.getText();
 				final String content = status.getUser().getName() + "::::" + tweet + "::::" + likes;
 				log.info(content);
 				producer.send(new ProducerRecord<>("tweets", content));
 			}
 
 			public void onDeletionNotice(StatusDeletionNotice statusDeletionNotice) {
-				log.debug("Status deletion: {}", statusDeletionNotice.getUserId());
 			}
 
 			public void onTrackLimitationNotice(int numberOfLimitedStatuses) {
-				log.debug("On notice: {}", numberOfLimitedStatuses);
 			}
 
 			public void onException(Exception ex) {
@@ -62,26 +55,21 @@ public class DemoTwitterKafkaProducerApplication {
 
 			@Override
 			public void onScrubGeo(long userId, long upToStatusId) {
-				log.debug("Geo status: {} - {}", userId, upToStatusId);
 			}
 
 			@Override
 			public void onStallWarning(StallWarning warning) {
-				log.warn("Warning: {}", warning.getMessage());
 			}
 
 			private long getLikes(Status status) {
 				return status.getRetweetedStatus() != null ? status.getRetweetedStatus().getFavoriteCount() : 0;
 			}
-
-			private String getTweet(Status status) {
-				final byte[] tweetInUtf8 = status.getText().getBytes(Charsets.UTF_8);
-				return new String(tweetInUtf8, Charsets.UTF_8);
-			}
 		};
 		twitterStream.addListener(listener);
+		final FilterQuery tweetFilterQuery = new FilterQuery();
+		tweetFilterQuery.track(new String[] { "Java" });
 		twitterStream.filter(tweetFilterQuery);
 		SpringApplication.run(DemoTwitterKafkaProducerApplication.class, args);
-		Runtime.getRuntime().addShutdownHook(new Thread(() -> producer.close()));
+		Runtime.getRuntime().addShutdownHook(new Thread(() -> producer.close())); //Kafka producer should close when application finishes.
 	}
 }
